0f316caaa84879f5af01f782ffb6f3918548bf43
jchang013 <44076844+jchang013@users.noreply.github.com>
2019-05-21 19:44:17 +0800

    Create registration.dart

---
+++ lib/registration.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+class RegistrationPage extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() => new _RegistrationPageState();
+
+}
+
