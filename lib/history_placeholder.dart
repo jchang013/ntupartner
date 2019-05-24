d37ae10b07ff8d11173977160a85edf1ff094ff0
jchang013 <44076844+jchang013@users.noreply.github.com>
2019-05-24 14:07:22 +0800

    Updated Match and History placeholder

--- lib/history_placeholder.dart
+++ lib/history_placeholder.dart
@@ -3,7 +3,11 @@ import 'package:flutter/material.dart';
 class PlaceholderHistory extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    final wordPair = 'No History Available';
-    return Text(wordPair, textAlign: TextAlign.center,);
+    //To add if else statement to control when there is a history or not, if there is history add in ListView
+    return new Container(
+      child: Center(
+        child: Text('No History Found')
+      )
+    );
   }
 }
--- lib/match_placeholder.dart
+++ lib/match_placeholder.dart
@@ -3,7 +3,24 @@ import 'package:flutter/material.dart';
 class PlaceholderUser extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    final wordPair = 'Test';
-    return Text(wordPair);
+    //To add if else statement to control when there is a daily match or not
+    return new Container(
+      child: new Column(
+        children: <Widget>[
+          new Image(
+            alignment: Alignment(0.0, -0.95),
+            image: new AssetImage('assets/default_user_img.png')
+          ),
+          new Align(
+            alignment: Alignment.centerLeft,
+            child: Text('<<Name>>', style: new TextStyle(fontSize: 20),),
+          ),
+          new Align(
+            alignment: Alignment.bottomCenter,
+            child: Text('<<Description>>', style: new TextStyle(fontSize: 16),)
+          )
+        ],
+      )
+    );
   }
 }
