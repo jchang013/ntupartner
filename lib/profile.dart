8fe0c68e70da7313ffc238b5e5c47f4e64e4831a
jchang013 <44076844+jchang013@users.noreply.github.com>
2019-06-07 21:04:18 +0800

    Created settings page with notification and match

---
+++ lib/profile.dart
@@ -0,0 +1,12 @@
+
+import 'package:flutter/material.dart';
+
+class ProfilePage extends StatefulWidget {
+  State<StatefulWidget> createState() {
+    return _ProfilePatgeState();
+  }
+}
+
+class _ProfilePageState extends State<ProfilePage> {
+
+}
--- lib/setting.dart
+++ lib/setting.dart
@@ -8,6 +8,19 @@ class SettingPage extends StatefulWidget {
 
 
 class _SettingPageState extends State<SettingPage> {
+  bool receiveMatch = true;
+  bool receiveNotification = true;
+
+  void _onMatchChanged(bool value) {
+    receiveMatch = false;
+    print('Stopped receiving matches');
+  }
+
+  void _onNotificationChanged(bool value) {
+    receiveNotification = false;
+    print('Stopped receiving notification');
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -16,15 +29,44 @@ class _SettingPageState extends State<SettingPage> {
             centerTitle: true,
             title: Text("Settings"),
           ),
-      body: Center(
-        child: Column(
-          //mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
+        body: Column(
 
+          children: <Widget>[
+            Row(
+              children: [
+                Text('\n')
+              ],
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                Text('Receive New Matches?', style: TextStyle(fontSize: 20),),
+
+                Switch(
+                  onChanged: _onMatchChanged,
+                  value: receiveMatch,
+                    ),
+                  ]
+                ),
+            Row(
+              children: [
+                Text('\n')
+              ],
+            ),
+            Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  Text('Receive Notification?', style: TextStyle(fontSize: 20),),
+
+                  Switch(
+                    onChanged: _onNotificationChanged,
+                    value: receiveMatch,
+                  ),
+                ]
+              ),
             ]
           )
         ),
-      )
-    );
+      );
   }
 }
