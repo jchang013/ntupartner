4ab050dd8b8c6df77ff53718d858204a240ad86e
jchang013 <44076844+jchang013@users.noreply.github.com>
2019-06-07 14:25:46 +0800

    Update setting.dart

--- lib/setting.dart
+++ lib/setting.dart
@@ -1,1 +1,28 @@
+import 'package:flutter/material.dart';
 
+class SettingPage extends StatefulWidget {
+  State<StatefulWidget> createState() {
+    return _SettingPageState();
+  }
+}
+
+
+class _SettingPageState extends State<SettingPage> {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+          appBar: AppBar(
+            title: Text("Settings"),
+          ),
+      body: Center(
+        child: Column(
+          //mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+          
+          ]
+        )
+      ),
+    );
+  }
+}
